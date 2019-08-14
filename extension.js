0947d50d63fdc1fad0fbd6b8f720808ad629627d
alx-ppv <alex.dimitrov.popov@gmail.com>
2019-08-14 16:03:39 +0300

    remove usage of registerOnConnectionComplete

--- extension.js
+++ extension.js
@@ -1,8 +1,6 @@
 const vscode = require("vscode");
 const azdata = require("azdata");
 
-let counter = 1;
-
 exports.activate = function(context) {
     let disposable = vscode.commands.registerCommand("deletedb.delete", function(context) {
         let dbName = context.nodeInfo.metadata.name;
@@ -29,36 +27,32 @@ function refreshDatabases(connectionId, nodePath) {
 
 function dropDatabase(connectionProfile, dbName) {
     return new Promise((resolve, reject) => {
-        let connectionProvider = azdata.dataprotocol.getProvider("MSSQL", azdata.DataProviderType.ConnectionProvider);
-        let connectionUri = `deletedb:${counter++}`;
-
-        connectionProvider.registerOnConnectionComplete(() => {
-            let queryProvider = azdata.dataprotocol.getProvider(
-                connectionProvider.providerId,
-                azdata.DataProviderType.QueryProvider
-            );
-
-            queryProvider.registerOnQueryComplete(result => {
-                connectionProvider.disconnect(connectionUri);
-                
-                if (!result.batchSummaries[0].hasError) {
-                    resolve();
-                }
-                else{
-                    vscode.window.showErrorMessage("Something has gone wrong");
-                    reject();
-                }
-            });
-
-            queryProvider.runQueryString(connectionUri, `DROP DATABASE "${dbName}"`).catch(error => {
-                vscode.window.showErrorMessage("Could not drop database :(");
-                console.log(error);
-            });
+        azdata.connection.getActiveConnections().then(x => {
+            console.log(x);
         });
 
-        connectionProvider.connect(connectionUri, connectionProfile).catch(error => {
-            vscode.window.showErrorMessage("Could not connect do database :(");
-            console.log(error);
+        azdata.connection.connect(connectionProfile, false, false).then(x => {
+            azdata.connection.getUriForConnection(x.connectionId).then(connectionUri => {
+                let queryProvider = azdata.dataprotocol.getProvider("MSSQL", azdata.DataProviderType.QueryProvider);
+                
+                queryProvider.registerOnQueryComplete(result => {
+                    let connectionProvider = azdata.dataprotocol.getProvider("MSSQL", azdata.DataProviderType.ConnectionProvider);
+                    connectionProvider.disconnect(connectionUri);
+                    if (!result.batchSummaries[0].hasError) {
+                        resolve();
+                    }
+                    else{
+                        vscode.window.showErrorMessage("Something has gone wrong");
+                        reject();
+                    }
+                });
+    
+                queryProvider.runQueryString(connectionUri, `DROP DATABASE "${dbName}"`).catch(error => {
+                    vscode.window.showErrorMessage("Could not drop database :(");
+                    reject();
+                    console.log(error);
+                });
+            });
         });
     });
 }

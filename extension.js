960ed36079ab7acaa4250f8a1b6db146d1ccf590
matan-levi <46105410+matan-levi@users.noreply.github.com>
2020-07-03 17:18:42 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -31,7 +31,7 @@ function dropDatabase(connectionProfile, dbName) {
             azdata.connection.getUriForConnection(connectionResult.connectionId).then(connectionUri => {
                 let queryProvider = azdata.dataprotocol.getProvider("MSSQL", azdata.DataProviderType.QueryProvider);
     
-                queryProvider.runQueryAndReturn(connectionUri, `DROP DATABASE "${dbName}"`)
+                queryProvider.runQueryAndReturn(connectionUri, `ALTER DATABASE "${dbName}" SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE "${dbName}"`)
                 .catch(error => {
                     //error with code 0 is thrown when query has nothing to return
                     let connectionProvider = azdata.dataprotocol.getProvider("MSSQL", azdata.DataProviderType.ConnectionProvider);
